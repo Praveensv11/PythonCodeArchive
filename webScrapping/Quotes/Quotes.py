9812e8074e63b2c403d3a4c54690a65e794f8d60
Praveen <praveensv799@gmail.com>
2024-04-08 21:26:47 +0530

    collecting all tags

--- webScrapping/Quotes/Quotes.py
+++ webScrapping/Quotes/Quotes.py
@@ -5,6 +5,18 @@ url = 'https://quotes.toscrape.com/'
 html_text = requests.get(url).text
 
 soup = BeautifulSoup(html_text, 'lxml')
+
+user_tags = input('Enter the tags name or enter "v" to view tags: ')
+
+all_tags = set()
+if user_tags == 'v':
+    tag = soup.find_all('a', class_='tag')
+    for i in tag:
+        print(i.text)
+        # print(i.a['href'])
+
+# print(all_tags)
+
 quotes = soup.find_all('span', class_="text")
 
 for quote in quotes:
