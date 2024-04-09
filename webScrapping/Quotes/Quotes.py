06a779eed74bcb034c2bd788996271b005c08089
Praveen <praveensv799@gmail.com>
2024-04-09 20:55:52 +0530

    collecting all tags

--- webScrapping/Quotes/Quotes.py
+++ webScrapping/Quotes/Quotes.py
@@ -6,19 +6,22 @@ html_text = requests.get(url).text
 
 soup = BeautifulSoup(html_text, 'lxml')
 
-user_tags = input('Enter the tags name or enter "v" to view tags: ')
-
 all_tags = set()
-if user_tags == 'v':
-    tag = soup.find_all('a', class_='tag')
-    for i in tag:
-        print(i.text)
-        # print(i.a['href'])
+while True:
+    user_tags = input('Enter the tags name or enter "v" to view tags: ')
 
-# print(all_tags)
+    if user_tags == 'v':
+        tag = soup.find_all('a', class_='tag')
+        for i in tag:
+            all_tags.add(i.text)
+        print(all_tags)
+    elif user_tags in all_tags:
+        print('done')
+        break
 
-quotes = soup.find_all('span', class_="text")
 
-for quote in quotes:
-    quote = quote.text.strip()
-    print(quote)
+# quotes = soup.find_all('span', class_="text")
+
+# for quote in quotes:
+#     quote = quote.text.strip()
+#     print(quote)
